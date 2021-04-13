e7583e76d2f71387137ebc1c3810b23d310f14a9
kimino98 <k.t.ynwa0105@gmail.com>
2021-04-13 17:05:24 +0900

    バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,13 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       redirect_to root_path
     else
-    render :edit
+      render :edit
     end
   end
 
- private
+  private
 
- def user_params
-   params.require(:user).permit(:name, :email)
- end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
-
